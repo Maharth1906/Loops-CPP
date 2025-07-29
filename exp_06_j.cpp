22e4945695c7ec01707b2d1482fdfe155da4b0b1
Maharth1906 <maharth.dhamanwala.btech2024@sitpune.edu.in>
2025-07-29 15:19:26 +0530

    exp_06_j.cpp

---
+++ exp_06_j.cpp
@@ -0,0 +1,40 @@
+//Maharth Dhamanwala
+//24070123154
+//B3
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string pass = "hi123";
+    string in;
+   
+    cout << "Enter the password: ";
+    cin >> in;
+
+    if (in.size() == pass.size())
+    {
+        for (int i = 0; i < pass.size(); i++)
+        {
+            if (pass[i] != in[i])
+            {
+                cout << "The password is wrong";
+            return 0;
+            }
+       
+            else
+            {
+                cout << "The password is correct";
+                return 0;
+            }
+        }
+    }
+    
+    else
+    {
+        cout << "The password is wrong";
+        return 0;
+    }
+}
