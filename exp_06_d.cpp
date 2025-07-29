3018831fd09f008d2e5ec449792f4fada39372e5
Maharth1906 <maharth.dhamanwala.btech2024@sitpune.edu.in>
2025-07-29 15:11:40 +0530

    exp_06_d.cpp

---
+++ exp_06_d.cpp
@@ -0,0 +1,16 @@
+//Maharth Dhamanwala
+//24070123154
+//B3
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 0; j < i; j++)
+            cout << "*";
+        cout << "\n";
+    }
+}
